bdeabb360f20859ab4f820c5c40a2fb7ea53598c
mnald12 <markronaldsicad@gmail.com>
2024-06-10 14:34:55 +0800

    add retrieve password

--- src/auth/SIgnin.js
+++ src/auth/SIgnin.js
@@ -1,14 +1,18 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import "./signin.css";
-import { useContext, useState } from "react";
+import { useContext, useEffect, useRef, useState } from "react";
 import { GrUserAdmin } from "react-icons/gr";
 import { FaBuildingUser } from "react-icons/fa6";
 import { Datas } from "../App";
 import { doc, getDoc } from "firebase/firestore";
 import { db } from "../db/config";
+import { RiCloseLine } from "react-icons/ri";
+import emailjs from "@emailjs/browser";
 
 const Signin = () => {
   const [isAdmin, setIsAdmin] = useState(false);
   const { setIsLogin, setRole } = useContext(Datas);
+  const [adminInfo, setAdminInfo] = useState({});
 
   const [adminUname, setAdminUname] = useState("");
   const [adminPwd, setAdminPwd] = useState("");
@@ -17,6 +21,48 @@ const Signin = () => {
 
   const [adminErr, setAdminErr] = useState(false);
   const [keeperErr, setKeeperErr] = useState(false);
+
+  const [isForgotPwdAdmin, setIsForgotPwdAdmin] = useState(false);
+
+  const form = useRef();
+
+  useEffect(() => {
+    const getInfo = async () => {
+      const docRef = doc(db, "users", "admin");
+      const docSnap = await getDoc(docRef);
+      const data = docSnap.data();
+      setAdminInfo(data);
+    };
+    getInfo();
+  }, [setAdminInfo]);
+
+  const sendAdminPwd = (e) => {
+    e.preventDefault();
+    emailjs
+      .sendForm(
+        "service_h155ilu",
+        "template_jjsbfwa",
+        form.current,
+        "JSQfi6h8QjsUi6kMY"
+      )
+      .then(
+        (result) => {
+          if (result) {
+            document.getElementById("pwdsent").classList.remove("d-none");
+            setTimeout(() => {
+              document.getElementById("pwdsent").classList.add("d-none");
+            }, 3000);
+          }
+        },
+        (error) => {
+          console.log(error.text);
+          document.getElementById("pwdfail").classList.remove("d-none");
+          setTimeout(() => {
+            document.getElementById("pwdfail").classList.add("d-none");
+          }, 3000);
+        }
+      );
+  };
 
   const login = async (mode, uname, pwd) => {
     if (mode === "admin") {
@@ -70,6 +116,14 @@ const Signin = () => {
 
   return (
     <div className="container">
+      <form ref={form} onSubmit={sendAdminPwd} hidden>
+        <input name="name" type="text" value={adminInfo.fullName} />
+        <input name="pwd" type="text" value={adminInfo.password} />
+        <input name="to_email" type="email" value={adminInfo.email} />
+        <button id="hbtn" type="submit">
+          send
+        </button>
+      </form>
       <div className={isAdmin ? "signin right-panel-active" : "signin"}>
         <div className="form-container admin-container">
           <form onSubmit={adminSubmitLogin}>
@@ -95,7 +149,12 @@ const Signin = () => {
               value={adminPwd}
               onChange={(e) => setAdminPwd(e.target.value)}
             />
-            <a href="#admin">Forgot your password?</a>
+            <a
+              style={{ cursor: "pointer" }}
+              onClick={() => setIsForgotPwdAdmin(true)}
+            >
+              Forgot your password?
+            </a>
             <button>Sign In</button>
           </form>
         </div>
@@ -123,8 +182,7 @@ const Signin = () => {
               value={keeperPwd}
               onChange={(e) => setKeeperPwd(e.target.value)}
             />
-            <a href="#admin">Forgot your password?</a>
-            <button>Sign In</button>
+            <button className="mt-12px">Sign In</button>
           </form>
         </div>
         <div className="overlay-container">
@@ -144,6 +202,42 @@ const Signin = () => {
               </button>
             </div>
           </div>
+        </div>
+      </div>
+      <div className={isForgotPwdAdmin ? "modal d-flex" : "modal d-none"}>
+        <div className="modal-body-delete">
+          <button
+            className="modal-close"
+            onClick={() => setIsForgotPwdAdmin(false)}
+          >
+            <RiCloseLine />
+          </button>
+          <p id="pwdsent" className="color-green d-none">
+            Password successfully sent to your email.
+          </p>
+          <p id="pwdfail" className="color-red d-none">
+            There was a problem sending your password to your email. Please try
+            again later
+          </p>
+          <h3>Forgot Password?</h3>
+          <p>
+            We will send your password to the email address associated with your
+            account.
+          </p>
+          <button
+            className="dbtns saves"
+            onClick={() => {
+              document.getElementById("hbtn").click();
+            }}
+          >
+            Send Password
+          </button>
+          <button
+            className="dbtns cancel"
+            onClick={() => setIsForgotPwdAdmin(false)}
+          >
+            Cancel
+          </button>
         </div>
       </div>
     </div>
